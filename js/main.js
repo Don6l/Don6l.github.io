0081a4f0e0b86613c2f70460c7445dc22085416f
don6l <donalmcclean@gmail.com>
2015-12-19 03:42:26 +0000

    Some more js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@
 $(document).ready(function(){
 	console.log('Begin');
 	$(document).on("click",'#wow',function(){wow()});
+	$(document).on("click", 'dl1', function(){dl1()});
 });
+
+var dl1 = function(){
+	alert("Hey Jasmine, I can see you too!");
+	window.open("https://www.facebook.com/LoveTheVisualSystem");
+};
 
 var wow = function(){
 	alert("Hey Aaron. I can see you");
