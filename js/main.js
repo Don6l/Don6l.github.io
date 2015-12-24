1704da920e12665323117f800fa68b05b0af7045
don6l <donalmcclean@gmail.com>
2015-12-24 17:24:33 +0000

    fas

--- js/main.js
+++ js/main.js
@@ -3,12 +3,31 @@
 */
 $(document).ready(function(){
 	console.log('Begin');
-	$(document).on("click",'#wow',function(){mainButton()});
-	$(document).on("click", '#dl1', function(){dl1()});
+	
 });
+
+var readyAll = function(){
+	console.log('readyAll');
+	$(document).on("click",'#mainButton',function(){mainButton()});
+	$(document).on("click", '#dl1', function(){dl1()});
+	$(document).on("click", '#dl2', function(){dl2()});
+	$(document).on("click", '#dl3', function(){dl3()});
+
+
+};
 
 var dl1 = function(){
 	alert("Download button one.");
+	
+};
+
+var dl2 = function(){
+	alert("Download button two.");
+	
+};
+
+var dl3 = function(){
+	alert("Download button three.");
 	
 };
 
